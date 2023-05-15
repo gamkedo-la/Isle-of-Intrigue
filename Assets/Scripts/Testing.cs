e89fd532dab0826e88a5850af877ff6f4d320824
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-05-15 22:37:01 +0500

    Added Grid class (WIP)

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid(4, 2);
+    }
+}
+
