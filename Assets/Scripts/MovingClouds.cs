d32249cbf2b0209eb1b72c913830134878ac8655
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-05-23 22:30:13 +0500

    Cloud Movement Implemented

--- Assets/Scripts/MovingClouds.cs
+++ Assets/Scripts/MovingClouds.cs
@@ -4,45 +4,46 @@ using UnityEngine;
 
 public class MovingClouds : MonoBehaviour
 {
-    public float minSpeed = 1f; // Minimum speed of cloud movement
-    public float maxSpeed = 3f; // Maximum speed of cloud movement
+    public float minSpeed = 1f;
+    public float maxSpeed = 3f;
 
-    private float cloudSpeed; // Actual speed of each cloud
-    private float cloudWidth; // Width of each cloud
-    private bool movingRight; // Direction of cloud movement
+    private float cloudSpeed;
+    private bool movingRight;
+
+    public GameObject skyRightEdge;
+    public GameObject skyLeftEdge;
+
+    float cloudWidth;
+
+    float skywidth;
+
+    Vector2 direction;
 
     private void Start()
     {
-        // Generate a random speed for each cloud
         cloudSpeed = Random.Range(minSpeed, maxSpeed);
 
-        // Get the width of each cloud
-        cloudWidth = GetComponent<SpriteRenderer>().bounds.size.x;
-
-        // Randomly set the initial direction of each cloud
         movingRight = Random.value > 0.5f;
+
+        direction = movingRight ? Vector2.right : Vector2.left;
+
     }
 
     private void Update()
     {
-        // Calculate the cloud's movement direction
-        Vector2 direction = movingRight ? Vector2.right : Vector2.left;
 
-        // Move the cloud horizontally
         transform.Translate(direction * cloudSpeed * Time.deltaTime);
 
+
         // Check if the cloud has moved off the screen
-        if (movingRight && transform.position.x > Screen.width + cloudWidth / 2f)
+        if (movingRight && transform.position.x > skyRightEdge.transform.position.x)
         {
             // Move the cloud to the left side of the screen
-            Vector2 newPosition = new Vector2(-cloudWidth / 2f, transform.position.y);
-            transform.position = newPosition;
+            direction = Vector2.left;
         }
-        else if (!movingRight && transform.position.x < -cloudWidth / 2f)
+        else if (!movingRight && transform.position.x < skyLeftEdge.transform.position.x)
         {
-            // Move the cloud to the right side of the screen
-            Vector2 newPosition = new Vector2(Screen.width + cloudWidth / 2f, transform.position.y);
-            transform.position = newPosition;
+            direction = Vector2.right;
         }
     }
 }
