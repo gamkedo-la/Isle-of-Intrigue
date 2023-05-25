65b0e60b7c080989805b8f65643f995d8011515a
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-05-26 01:24:41 +0500

    Added Enemy Movement

--- Assets/Scripts/EnemyAI/MovementState.cs
+++ Assets/Scripts/EnemyAI/MovementState.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class MovementState : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform target;
+    public float speed = 20;
+
+    Vector2[] path;
+    int targetIndex;
+
     void Start()
     {
-        
+        StartCoroutine(RefreshPath());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RefreshPath()
     {
-        
+        Vector2 targetPositionOld = (Vector2)target.position + Vector2.up; // ensure != to target.position initially
+
+        while (true)
+        {
+            if (targetPositionOld != (Vector2)target.position)
+            {
+                targetPositionOld = (Vector2)target.position;
+
+                path = Pathfinding.RequestPath(transform.position, target.position);
+                StopCoroutine("FollowPath");
+                StartCoroutine("FollowPath");
+            }
+
+            yield return new WaitForSeconds(.25f);
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        if (path.Length > 0)
+        {
+            targetIndex = 0;
+            Vector2 currentWaypoint = path[0];
+
+            while (true)
+            {
+                if ((Vector2)transform.position == currentWaypoint)
+                {
+                    targetIndex++;
+                    if (targetIndex >= path.Length)
+                    {
+                        yield break;
+                    }
+                    currentWaypoint = path[targetIndex];
+                }
+
+                transform.position = Vector2.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+                yield return null;
+
+            }
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (path != null)
+        {
+            for (int i = targetIndex; i < path.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                //Gizmos.DrawCube((Vector3)path[i], Vector3.one *.5f);
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine(transform.position, path[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(path[i - 1], path[i]);
+                }
+            }
+        }
     }
 }
