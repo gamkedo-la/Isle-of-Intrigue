6e720b335db9c21398b092031696573899858083
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-06-03 03:28:25 +0500

    Added Improved Dagger

--- Assets/Scripts/Grenade.cs
+++ Assets/Scripts/Grenade.cs
@@ -7,22 +7,24 @@ public class Grenade : MonoBehaviour
     public GameObject explosionVfx;
     public float vfxDestroyDelay = 1f;
 
+    int playerLayer;
+    int grenadeLayer;
+
     void Start()
     {
-        int playerLayer = LayerMask.NameToLayer("Player");
-        int grenadeLayer = LayerMask.NameToLayer("Grenade");
+        playerLayer = LayerMask.NameToLayer("Player");
+        grenadeLayer = LayerMask.NameToLayer("Grenade");
     }
 
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (other.gameObject.layer == playerLayer || other.gameObject.layer == grenadeLayer)
         {
-            // Ignore collision with player, do not instantiate VFX
             return;
-
         }
-        Destroy(gameObject, 1f);
+
+        Destroy(gameObject);
         GameObject explosion = Instantiate(explosionVfx, other.contacts[0].point, transform.rotation);
         Destroy(explosion, vfxDestroyDelay);
 
