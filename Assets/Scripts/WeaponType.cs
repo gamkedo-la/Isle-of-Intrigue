0f9170b0e9650baead6025ec0b64097c233e744d
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-07-07 22:20:14 +0500

    Updated with layer weight code

--- Assets/Scripts/WeaponType.cs
+++ Assets/Scripts/WeaponType.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class WeaponType : MonoBehaviour
 {
+
     public List<GameObject> weapons = new List<GameObject>();
+
     public GameObject pistolPrefab;
     public GameObject riflePrefab;
     public GameObject rocketLauncherPrefab;
+
+    public Animator animator;
 
     public enum WeaponState
     {
@@ -17,9 +21,7 @@ public class WeaponType : MonoBehaviour
         RocketLauncher,
     }
 
-
     public WeaponState currentWeaponType;
-
 
 
     public void ChangeWeaponState(WeaponState targetWeaponState)
@@ -39,9 +41,18 @@ public class WeaponType : MonoBehaviour
             if (weapon.CompareTag(currentWeaponTag))
             {
                 weapon.SetActive(true);
+                SetAnimationLayerWeight(currentWeaponTag, 1);
                 Debug.Log("matched");
                 break;
             }
         }
+    }
+
+    private void SetAnimationLayerWeight(string layerName, int weight)
+    {
+        int layerIndex = animator.GetLayerIndex(layerName);
+        Debug.Log(layerName);
+        animator.SetLayerWeight(layerIndex, weight);
+        Debug.Log(layerIndex);
     }
 }
