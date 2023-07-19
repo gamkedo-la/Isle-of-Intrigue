efa875316c2ffc5359cecef7cf2af9852014965c
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-07-19 21:00:25 +0500

    Bullet direction issue resolved

--- Assets/Scripts/EnemyAI/EnemyShooting.cs
+++ Assets/Scripts/EnemyAI/EnemyShooting.cs
@@ -7,12 +7,21 @@ public class EnemyShooting : MonoBehaviour
 
     public Transform player;
     public GameObject bulletPrefab;
-
     public Transform shootingPoint;
+
+    public float bulletSpeed = 10f;
+
+    Rigidbody2D rigid;
+
 
     public Transform container;
     public float shootCooldown = 1.0f;
     public float shootDistance = 10.0f;
+
+    void Start()
+    {
+        rigid = bulletPrefab.GetComponent<Rigidbody2D>();
+    }
 
 
     void Update()
@@ -28,6 +37,8 @@ public class EnemyShooting : MonoBehaviour
         if (distance <= shootDistance)
         {
             GameObject bullet = Instantiate(bulletPrefab, shootingPoint.position, shootingPoint.rotation);
+            Vector2 direction = (player.transform.position - transform.position).normalized;
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
             bullet.transform.parent = container;
         }
 
--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
     public float bulletSpeed = 10f;
-    public float bulletSpray = 45f;
+    // public float bulletSpray = 45f;
 
     Rigidbody2D rigid;
+    Vector2 direction;
 
+    private GameObject player;
 
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         rigid = GetComponent<Rigidbody2D>();
         Shoot();
     }
 
     public void Shoot()
     {
-        Vector2 direction = (transform.forward - transform.position).normalized;
+        direction = (player.transform.position - transform.position).normalized;
         rigid.velocity = direction * bulletSpeed;
     }
 }
