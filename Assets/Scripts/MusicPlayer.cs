6171fde20b2b7abe4b363f1fc6faac3b66186ac5
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-07-28 20:57:43 +0500

    Added Music into looping pause script

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public float pauseDuration = 5f;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        PlayMusic();
+    }
+
+    void PlayMusic()
+    {
+        audioSource.Play();
+        StartCoroutine(LoopMusicWithPause());
+    }
+
+    IEnumerator LoopMusicWithPause()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(28);
+            audioSource.Pause();
+            yield return new WaitForSeconds(pauseDuration);
+            audioSource.UnPause();
+        }
+    }
+}
