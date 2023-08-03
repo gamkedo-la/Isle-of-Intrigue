1edf7b5c08a7c489427b5eba4372558c71268b9d
bilal <bilal.cheema@frag-games.com>
2023-08-04 03:01:41 +0500

    Improved start menu and added button functionality.

---
+++ Assets/Scripts/UI/OnButtonClick.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OnButtonClick : MonoBehaviour
+{
+
+    public enum ButtonFunctions
+    {
+        PLAY,
+        OPTIONS,
+        CREDITS
+    }
+    public ButtonFunctions function;
+
+    void Start()
+    {
+        switch (function)
+        {
+            case ButtonFunctions.PLAY:
+                GetComponent<Button>().onClick.AddListener(Play);
+                break;
+            case ButtonFunctions.OPTIONS:
+                GetComponent<Button>().onClick.AddListener(Options);
+                break;
+            case ButtonFunctions.CREDITS:
+                GetComponent<Button>().onClick.AddListener(Credits);
+                break;
+        }
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    void Options()
+    {
+        Debug.Log("Clicked Options");
+    }
+
+    void Credits()
+    {
+        Debug.Log("Clicked Credits");
+    }
+}
