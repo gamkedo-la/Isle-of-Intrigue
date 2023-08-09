bb6d59d24735fccc7b14482680056f8a38385d3a
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-08-09 21:13:00 +0500

    PlayerSpawn resolved

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -10,21 +10,10 @@ public class PlayerSpawner : MonoBehaviour
     public AudioClip spawnSound;
 
 
-    private void Start()
-    {
-        spawnPos.position = new Vector3(0f, 0f, 0f);
-    }
 
-    private void Update()
-    {
-        if (!health.DieStatus())
-        {
-            spawnPos.position = new Vector2(player.position.x, 0);
-        }
-    }
 
     public void SpawnPlayer()
     {
-        player.position = new Vector2(spawnPos.position.x,32.3f);
+        player.position += Vector3.up * 5.0f;
     }
 }
