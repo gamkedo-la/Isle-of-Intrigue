1514735cba7ac13c0c842c878c758e97049752be
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-08-10 02:43:47 +0500

    Done wit

--- Assets/Scripts/Player/WeaponAiming.cs
+++ Assets/Scripts/Player/WeaponAiming.cs
@@ -6,6 +6,7 @@ public class WeaponAiming : MonoBehaviour
 {
     public Transform headBone;
     public Transform handsBone;
+    public Transform aimer; // a target reticle that follows the mouse
 
     public float headAimingSpeed = 10f;
     public float handAimingSpeed = 10f;
@@ -16,8 +17,8 @@ public class WeaponAiming : MonoBehaviour
     public float minHeadAngle = -90f;
     public float maxHeadAngle = 90f;
 
-    public float minHandAngle = -90f;
-    public float maxHandAngle = 90f;
+    public float minHandAngle = -90f+45f;
+    public float maxHandAngle = 90f+45f;
 
     private float currentHeadRotationAngle;
     private float currentHandRotationAngle;
@@ -30,11 +31,23 @@ public class WeaponAiming : MonoBehaviour
 
     private void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePosition.z = 0f;
+        // old way which doesn't include z so the world pos is far away
+        // Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // mousePosition.z = 0f;
 
-        Vector3 lookDirection = mousePosition - transform.position;
-        currentHeadRotationAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        //Vector3 firePosition = transform.position; // ERROR: this is near the feet
+        //Vector3 firePosition = handsBone.position; // ERROR: this is near the knees
+        Vector3 firePosition = transform.position;
+        // centered on the chest! FIXME: actually grab gun barrel or shoulder pos?
+        firePosition.y += 2.5f;
+        firePosition.x -= 0.25f;
+
+        // the 10f is "units from the camera" to match the camera position
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0f));//firePosition.z)); 
+        mousePosition.z = firePosition.z;
+
+        Vector3 lookDirection = mousePosition - firePosition;
+        currentHeadRotationAngle = 90f + (Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg);
 
         currentHeadRotationAngle = Mathf.Clamp(currentHeadRotationAngle, minHeadAngle, maxHeadAngle);
 
@@ -43,7 +56,19 @@ public class WeaponAiming : MonoBehaviour
         // Calculate the normalized aiming angle for the hands
         float normalizedHandAimAngle = Mathf.InverseLerp(minHeadAngle, maxHeadAngle, currentHeadRotationAngle);
         currentHandRotationAngle = Mathf.Lerp(minHandAngle, maxHandAngle, normalizedHandAimAngle);
+        handsBone.localRotation = Quaternion.Euler(0f, 0f, currentHandRotationAngle+(45f*Mathf.Deg2Rad));
 
-        handsBone.localRotation = Quaternion.Euler(0f, 0f, currentHandRotationAngle);
+        //handsBone.rotation = Quaternion.Euler(0f, 0f, currentHeadRotationAngle+(90f*Mathf.Deg2Rad));
+        
+        // lol silly but effective! just oriented strangely doe to handbone being at feet pos
+        // handsBone.transform.LookAt(mousePosition);
+
+        // aiming reticle - useful for debugging
+        if (aimer) aimer.transform.position = mousePosition;
+
+        // for debug only
+        Color color = new Color(1f,0f,0f,1f);
+        Debug.DrawLine(firePosition, mousePosition, color);
+
     }
 }
