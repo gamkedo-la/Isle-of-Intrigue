2db8fcbdffbc822d4af1e1d9aef3678f32fe031f
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-08-10 20:19:19 +0500

    Reward Badge issue resolved

--- Assets/Scripts/RewardBadgePickUp.cs
+++ Assets/Scripts/RewardBadgePickUp.cs
@@ -10,7 +10,7 @@ public class RewardBadgePickUp : MonoBehaviour
 
   
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
