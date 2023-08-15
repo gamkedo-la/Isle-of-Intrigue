8602b6ca3414f3971fffc380fbc7b7e1c636f9a8
Syed Daniyal Ali <54919310+daniyal-ali613@users.noreply.github.com>
2023-08-15 20:38:09 +0500

    Fixed the Rocket Issue

--- Assets/Scripts/RocketProjectile.cs
+++ Assets/Scripts/RocketProjectile.cs
@@ -16,5 +16,6 @@ public class RocketProjectile : MonoBehaviour
     {
         GameObject vfx = Instantiate(blastVfx, collision.transform.position, Quaternion.identity);
         Destroy(vfx, 1f);
+        gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
     }
 }
